f1782eb721bdf165621a3f226f20e48f49fc5938
maqsudinamdar <maqsudinamdar@gmail.com>
2021-10-01 15:31:02 +0530

    feat (solved) : find target efficiently problem

---
+++ find_target.rb
@@ -0,0 +1,47 @@
+class FindTarget
+
+    attr_reader :array, :target
+    attr_accessor :dp, :result, :index
+    
+    def initialize(array, target)
+        @array = array
+        @target = target
+        @dp = []
+        @result = []
+        @index = []
+    end
+
+    def print_log        
+        print self.result
+        puts
+    end
+
+    def solve
+        for i in 0...self.array.length do
+
+            self.index[self.array[i]] = i if self.index[self.array[i]].nil?
+
+            if self.array[i] < self.target
+                if !self.dp[self.array[i]].nil?
+                    self.result = [index[self.dp[self.array[i]]], index[self.array[i]]]
+                    break
+                end
+                self.dp[target-self.array[i]] = self.array[i]
+            else
+                self.dp[i] = -1
+            end
+        end
+    end
+
+    def calculate
+        solve
+        print_log
+    end
+
+end
+
+numbers = [1,2,3,5,2]
+target = 4
+
+calc = FindTarget.new(numbers, target)
+calc.calculate
